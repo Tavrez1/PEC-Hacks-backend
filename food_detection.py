e844aacdc54ee89afbcadf810bb76fb1cd18c895
Tavrez1 <147392863+Tavrez1@users.noreply.github.com>
2025-12-27 09:23:37 +0530

    added env for apikey

--- food_detection.py
+++ food_detection.py
@@ -7,7 +7,7 @@ import io
 
 # --- CONFIGURATION ---
 # Replace with your actual API key or set it as an environment variable
-API_KEY = "AIzaSyBbhTMDkyZ0j9lay0aBuMAOgGN1mm4XoAs"
+API_KEY = os.getenv("API_KEY")
 
 def analyze_food_image(image):
     client = genai.Client(api_key=API_KEY)
